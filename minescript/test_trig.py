eaeaa3b9064adc88dbefd5c1b8425d94fc794b1f
imthabomb <113074112+imthabomb@users.noreply.github.com>
2026-04-14 19:17:03 +0000

    f

--- minescript/test_trig.py
+++ minescript/test_trig.py
@@ -1,8 +1,11 @@
+import math
+
 def trig_time(target_x, target_y, target_z):
-    player_pos = m.player_position()  # Call the function to get the position
-    dx = target_x - player_pos[0]
-    dy = target_y - player_pos[1]
-    dz = target_z - player_pos[2]
+      # Call the function to get the position
+    dx = target_x - 0
+    dy = target_y - 0
+    dz = target_z - 0
+    
     horizontal_dist = math.sqrt(dx**2 + dz**2)
     pitch = math.atan2(dy, horizontal_dist) * (180 / math.pi)
     yaw = math.atan2(dx, dz) * (180.0 / math.pi)
