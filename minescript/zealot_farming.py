94b5407cf1a7f9a94662b39108d28fca9ef9e958
imthabomb <113074112+imthabomb@users.noreply.github.com>
2026-04-14 13:53:24 -0700

    zealot farming works now

--- minescript/zealot_farming.py
+++ minescript/zealot_farming.py
@@ -55,7 +55,7 @@ def trig_time(target_x, target_y, target_z):
     dy = target_y - player_pos[1]
     dz = target_z - player_pos[2]
     horizontal_dist = math.sqrt(dx**2 + dz**2)
-    pitch = math.atan2(dy, horizontal_dist) * (180 / math.pi)
+    pitch = math.atan2(dy, dx) * (180 / math.pi)
     yaw = math.atan2(dx, dz) * (180.0 / math.pi)
     return yaw, pitch
 
